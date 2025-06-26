810640c40812261db5a4d372b6259b3d9cddd025
Matthew McLeod <matt@sparkboom.com>
2025-06-25 22:25:14 -0700

    Add Mac App Store apps

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
           k9s
           kubectl
           kubernetes-helm
+          mas
           nmap
           nodejs
           openssl_3
@@ -73,7 +74,6 @@
           "little-snitch"
           "nordvpn"
           "obsidian"
-          "omnifocus"
           #  openvpn-connect
           # postman
           "proton-pass"
@@ -101,6 +101,22 @@
           "whatsapp"
           "zoom"
         ];
+
+        masApps = {
+          "3d Scanner App" = 1419913995;
+          "AI Chat Bot" = 6447312365;
+          "Apple Configurator" = 1037126344;
+          "Brother P-touch Editor" = 1453365242;
+          "Day One" = 1055511498;
+          "Home Assistant" = 1099568401;
+          "Numbers" = 409203825;
+          "OmniFocus" = 1542143627;
+          "Paprika Recipe Manager 3" = 1234567890;
+          "Pixelmator Pro" = 1289583905;
+          "Spatial Media Toolkit" = 6477903679;
+          "Windows App" = 1295203466;
+          "Xcode" = 497799835;
+        };
         # onActivation.cleanup = false; # comment out to disable cleanup
       };
 
