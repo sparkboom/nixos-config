f40c291cdaeeac60a40fc24ee040954ed8f7418b
Matthew McLeod <matt@sparkboom.com>
2025-07-09 07:53:27 -0700

    improve welcome message

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -40,7 +40,7 @@
     # Enable fuzzy zsh-autosuggestions
     source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
 
-    cowsay "Welcome mooo."
+    cowsay "Hi $USER! Welcome to $(hostname) mooo."
   '';
 
   history = {
