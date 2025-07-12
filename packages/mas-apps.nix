b7e2e455e2210e06e4bed908033e6af2f0183eaa
Matthew McLeod <matt@sparkboom.com>
2025-07-11 19:01:29 -0700

    Add some missing mas apps

--- packages/mas-apps.nix
+++ packages/mas-apps.nix
@@ -1,10 +1,13 @@
 {
   "3d Scanner App"            = 1419913995; # 3D scanning utility by Apple
   "AI Chat Bot"               = 6447312365; # AI-powered chat bot app
+  "Amphetamine"               = 937984704;  # Prevents Mac from sleeping
   "Apple Configurator"        = 1037126344; # Apple device configuration tool
   "Brother P-touch Editor"    = 1453365242; # Label editor for Brother printers
   "Day One"                   = 1055511498; # Journal and diary app
   "Home Assistant"            = 1099568401; # Home automation dashboard
+  "IPTV Expert"               = 1442201240; # IPTV streaming app
+  "Lensa"                     = 1436779680; # AI photo editing app
   "Numbers"                   = 409203825;  # Apple spreadsheet app
   "OmniFocus"                 = 1542143627; # Task management app
   "Paprika Recipe Manager 3"  = 1234567890; # Recipe manager and meal planner
