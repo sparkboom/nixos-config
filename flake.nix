86d82daecf2f479b6ddc722fbc9c6342329f010f
Matthew McLeod <matt@sparkboom.com>
2025-07-15 20:10:32 -0700

    Refactor code to host and user

--- flake.nix
+++ flake.nix
@@ -20,72 +20,9 @@
       mac-app-util,
       home-manager,
     }:
-  let
-    configuration = { pkgs, ... }: {
-      nixpkgs.config.allowUnfree = true;
-      environment.systemPackages = import ./nix/packages/system-packages.nix { inherit pkgs; };
-
-      # # Homebrew casks
-      homebrew = {
-        enable = true;
-        casks = import ./nix/packages/homebrew-casks.nix;
-        masApps = import ./nix/packages/mas-apps.nix;
-        onActivation.cleanup = "uninstall";
-        onActivation.upgrade = false;
-        onActivation.autoUpdate = true;
-      };
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.zsh.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # Security
-      security.pam.services.sudo_local.touchIdAuth = true;
-
-      system.primaryUser = "matt";
-
-      system.defaults = {
-        dock.autohide = false;
-        finder.AppleShowAllExtensions = true;
-        finder.FXPreferredViewStyle = "clmv"; # default Finder to column view
-        # screencapture.location = "~/Desktop/screenshots";
-        # screensaver.askForPasswordDelay = 10;
-      };
-
-      users.users.matt = {
-        name = "matt";
-        home = "/Users/matt";
-      };
-
-      fonts.packages = import ./nix/packages/fonts.nix { inherit pkgs; };
-    };
-  in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#MattsM3
-    darwinConfigurations.MattsM3 = nix-darwin.lib.darwinSystem {
-      modules = [
-        mac-app-util.darwinModules.default
-        configuration
-        home-manager.darwinModules.home-manager {
-          home-manager.backupFileExtension = ".bak";
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.matt = import ./nix/home-manager/home.nix;
-        }
-      ];
+    darwinConfigurations.MattsM3 = import ./nix/hosts/MattsM3/darwin.nix {
+      inherit self nix-darwin mac-app-util home-manager nixpkgs;
     };
 
     # Expose package set, including overlays, for convinience.
---
+++ nix/hosts/MattsM3/darwin.nix
@@ -0,0 +1,77 @@
+{ 
+  self, 
+  nix-darwin, 
+  mac-app-util,
+  home-manager,
+  nixpkgs,
+}:
+let
+  configuration = { pkgs, ... }: {
+    nixpkgs.config.allowUnfree = true;
+    environment.systemPackages = import ./packages/system-packages.nix { inherit pkgs; };
+
+    # # Homebrew casks
+    homebrew = {
+      enable = true;
+      casks = import ./packages/homebrew-casks.nix;
+      masApps = import ./packages/mas-apps.nix;
+      onActivation.cleanup = "uninstall";
+      onActivation.upgrade = false;
+      onActivation.autoUpdate = true;
+    };
+
+    # Install fonts
+    fonts.packages = import ./packages/fonts.nix { inherit pkgs; };
+
+    # Necessary for using flakes on this system.
+    nix.settings.experimental-features = "nix-command flakes";
+
+    # Enable alternative shell support in nix-darwin.
+    programs.zsh.enable = true;
+
+    # Set Git commit hash for darwin-version.
+    system.configurationRevision = self.rev or self.dirtyRev or null;
+
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 6;
+
+    # The platform the configuration will be used on.
+    nixpkgs.hostPlatform = "aarch64-darwin";
+
+    # Security
+    security.pam.services.sudo_local.touchIdAuth = true;
+
+    # System configuration
+    system.defaults = {
+      dock.autohide = false;
+      finder.AppleShowAllExtensions = true;
+      finder.FXPreferredViewStyle = "clmv";
+      # default Finder to column view
+      # screencapture.location = "~/Desktop/screenshots";
+      # screensaver.askForPasswordDelay = 10;
+    };
+
+    system.primaryUser = "matt";
+
+    users.users.matt = {
+      name = "matt";
+      home = "/Users/matt";
+    };
+  };
+in
+nix-darwin.lib.darwinSystem {
+  modules = [
+    mac-app-util.darwinModules.default
+    configuration
+    home-manager.darwinModules.home-manager {
+      home-manager = {
+        backupFileExtension = ".bak";
+        useGlobalPkgs = true;
+        useUserPackages = true;
+
+        users.matt = import ./users/matt/home.nix;
+      };
+    }
+  ];
+}
