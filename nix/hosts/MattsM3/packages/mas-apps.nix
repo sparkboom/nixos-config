7fb00084e0be38b2c9a5dd49aeabee93ed77a0a1
Matthew McLeod <matt@sparkboom.com>
2025-07-28 15:39:39 -0700

    Add perplexity to mas apps on m3

--- nix/hosts/MattsM3/packages/mas-apps.nix
+++ nix/hosts/MattsM3/packages/mas-apps.nix
@@ -11,6 +11,7 @@
   "Numbers"                   = 409203825;  # Apple spreadsheet app
   "OmniFocus"                 = 1542143627; # Task management app
   "Paprika Recipe Manager 3"  = 1234567890; # Recipe manager and meal planner
+  "Perplexity: Ask Anything"  = 6714467650; # AI-powered search and Q&A tool
   "Pixelmator Pro"            = 1289583905; # Image editor
   "Spatial Media Toolkit"     = 6477903679; # Spatial media processing tools
   "Windows App"               = 1295203466; # Microsoft Remote Desktop
